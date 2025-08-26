9f377243e3f6667a00c640ed5a79a709456abc2a
andhu47 <andhuxx@gmail.com>
2025-08-26 16:30:55 +0500

    Create supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,6 @@
+// Fill with your project’s values from Project Settings → API
+const cfg = {
+  url: "https://uygkbchzkfwwyuyraevn.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV5Z2tiY2h6a2Z3d3l1eXJhZXZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYyMDY0MTksImV4cCI6MjA3MTc4MjQxOX0.EXNWPByCYHAA3yare0DsPY6gbjIUTyJrJ9MLHU_dlMk"
+};
+export default cfg;
